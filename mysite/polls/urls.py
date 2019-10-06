b35d5432a7603fbe0c5ae3d05ce80195eb5d4558
onigirionta <onigirionta@gmail.com>
2019-10-07 01:24:20 +0300

    shit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('futures', views.futures, name='futures'),
+    path('futures/<name>', views.modify_futures),
     path('trades', views.trades, name='trades'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.core.serializers import serialize
 
@@ -8,8 +10,31 @@ import json
 from .models import Futures, Trades
 
 def futures(request):
-    fs = serialize('json', Futures.objects.all())
-    return HttpResponse(f'{{"data": {fs}}}')
+    if request.method == 'GET':
+        fs = serialize('json', Futures.objects.all())
+        return HttpResponse(f'{{"data": {fs}}}')
+    elif request.method == 'POST':
+        body = json.load(request)
+        name = body['name']
+        code = body['code']
+        date = datetime.strptime(body['date'], '%Y-%m-%d')
+        futures = Futures(name=name, base=code, exec_date=date)
+        futures.save()
+        return HttpResponse('futures created')
+    else:
+        pass  # TODO: error
+
+def modify_futures(request, name):
+    if request.method == 'DELETE':
+        Futures.objects.filter(name=name).delete()
+        return HttpResponse('futures deleted')
+    elif request.method == 'PUT':
+        body = json.load(request)
+        new_name = body['name']
+        code = body['code']
+        date = datetime.strptime(body['date'], '%Y-%m-%d')
+        Futures.objects.filter(name=name).update(name=new_name, base=code, exec_date=date)
+        return HttpResponse('futures updated')
 
 def trades(request):
     ts = serialize('json', Trades.objects.all())
